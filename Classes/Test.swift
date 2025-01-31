3a4980dcd8fcd3156e761ec5ca7a1a42b82ae6d9
Tala Akash <akasht.noble@gmail.com>
2025-01-31 14:44:04 +0530

    -- Initial commit

---
+++ Classes/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  Pods
+//
+//  Created by Akash Tala on 31/01/25.
+//
+
+public class Test {
+    init() { }
+    
+    public func logMessage() {
+        print("Hello user welcome to pod")
+    }
+}
--- Example/TemplateEditor/ViewController.swift
+++ Example/TemplateEditor/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TemplateEditor
 
 class ViewController: UIViewController {
 
